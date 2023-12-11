14d504a18b164d2d15982af1d969ef4783531f6a
Dharmendra Yeole <126152052+dharmendrayeole@users.noreply.github.com>
2023-12-11 16:45:23 +0530

    Update appp.py

--- appp.py
+++ appp.py
@@ -2,8 +2,10 @@ import streamlit as st
 import pickle
 import string
 from nltk.corpus import stopwords
-import nltk
 from nltk.stem.porter import PorterStemmer
+import nltk
+nltk.download('punkt')
+
 
 ps = PorterStemmer()
 
